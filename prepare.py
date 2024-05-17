b704f2a36f1cfdd0bd95e01c12502db4c388aab9
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-05-18 00:32:27 +0300

    added `move` command for move sound to different category

--- prepare.py
+++ prepare.py
@@ -5,7 +5,9 @@ from filedefines import *
 from dotenv import load_dotenv
 from pathlib import Path
 from pydub import AudioSegment
+from tqdm import tqdm
 
+# set the directory in the same place as the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
@@ -32,29 +34,61 @@ if not sounds_path or not len(sounds_path) > 0:
     print("\033[91mSOUNDS_PATH in empty.\033[0m")
     exit()
 
+# initialize the dictionary
 data = {}
 
-for main_dir, main_dir_props in dict(table).items():
-    dir_path = os.path.join(sounds_path, main_dir)
-    data[main_dir] = {}
-    print(main_dir)
+for category in fakecategories:
+    data[category] = {}
+
+for category, _ in dict(table).items():
+    data[category] = {}
+
+# initializing the folder structure
+for category in data:
+    os.makedirs(os.path.join(script_dir, "soundpack", category), exist_ok=True)
+
+# converting files to a compressed format and moving them to the mod folder
+for category, category_props in dict(table).items():
+    if category in fakecategories:
+        continue
+
+    dir_path = os.path.join(sounds_path, category)
+    print(category)
+
+    if 'move' in category_props:
+        orig_category_name = category
+    else:
+        # This is probably not entirely correct, but... I'm going to use this variable in the same way as a flag, then it must be declared
+        orig_category_name = False
 
     for root, _, files in os.walk(dir_path):
         files = sorted(files)
+        bar = tqdm(total=len(files), ncols=100, position=0, leave=True)
+
         for file in files:
+            bar.update(1)
             name = Path(file).stem
-            if ('exclude' in main_dir_props and not name in main_dir_props['exclude']) or 'exclude' not in main_dir_props:
+            if ('exclude' in category_props and not name in category_props['exclude']) or 'exclude' not in category_props:
+                if orig_category_name and name in category_props['move']:
+                    category = category_props['move'][name]
+
                 file_path = os.path.join(root, file)
 
-                output_path = os.path.join(script_dir, "soundpack", main_dir, f"{name}.ogg")
+                output_path = os.path.join(script_dir, "soundpack", category, f"{name}.ogg")
                 if file_path.endswith('.wav') and not os.path.exists(output_path):
                     sound = AudioSegment.from_wav(file_path)
 
                     os.makedirs(os.path.dirname(output_path), exist_ok=True)
                     sound.export(output_path, format="ogg")
-                elif os.path.exists(output_path):
-                    data[main_dir][name] = f"{name}.ogg"
 
+                if os.path.exists(output_path):
+                    data[category][name] = f"{name}.ogg"
+
+                if orig_category_name:
+                    category = orig_category_name
+        bar.close()
+
+# generating a list of files
 tab = "    "
 with open(os.path.join(script_dir, "soundpack", "files.lua"), "w") as f:
     f.writelines([
@@ -62,18 +96,18 @@ with open(os.path.join(script_dir, "soundpack", "files.lua"), "w") as f:
         '———————————No io access?———————————\n',
         '⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝\n',
         '⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇\n',
-        '⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀\n',
-        '⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀\n',
-        '⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀\n',
-        '⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀\n',
-        '⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀\n',
-        '⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀\n',
-        '⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n',
-        '⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n',
-        '⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n',
-        '⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n',
-        '⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n',
-        '—————————————————————————————\n]]\n',
+        '⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏\n',
+        '⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁\n',
+        '⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉\n',
+        '⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂\n',
+        '⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂\n',
+        '⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁\n',
+        '⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏\n',
+        '⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝\n',
+        '⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟\n',
+        '⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟\n',
+        '⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋\n',
+        '———————————————————————————————————\n]]\n',
     ])
     f.write("return {\n")
     for folder_name, folder_content in data.items():
@@ -85,6 +119,7 @@ with open(os.path.join(script_dir, "soundpack", "files.lua"), "w") as f:
         f.write(tab+"},\n")
     f.write("}")
 
+# localization generation
 mod_base = "hl-soundpack/"
 mod_base_translate = "Half-Life/"
 with open(os.path.join(script_dir, "locale/en/generated-categories.cfg"), "w") as fcat:
